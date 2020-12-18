5a6172166fcb02fcce335a7daeb857796df7af40
edisdev <hedis285@gmail.com>
2020-12-18 19:07:23 +0300

    Add image loader config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const path = require('path')
 
 module.exports = {
+  images: {
+    loader: 'imgix'
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'src/assets/stylesheets')],
   }
